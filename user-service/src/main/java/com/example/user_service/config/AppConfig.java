63af93185688a7908bf378ba1fdf83efe81b3944
dbehdrbs0806 <a67422830@gmail.com>
2025-05-15 10:14:22 +0900

    security 해제 및 k8s 도입 deployment와 service등 yaml 작성

--- user-service/src/main/java/com/example/user_service/config/AppConfig.java
+++ user-service/src/main/java/com/example/user_service/config/AppConfig.java
@@ -3,10 +3,12 @@ package com.example.user_service.config;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 // Bean Config
 @Configuration
+// @EnableWebSecurity
 public class AppConfig {
 
     // 비밀번호 암호화 Bean 사용
